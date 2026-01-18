48d65ae5010c768ef1342e139f1d3b42df3af684
Edmond-Mawuli <edmondasoe123@gmail.com>
2026-01-18 23:26:08 +0000

    Update script file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,12 +1,14 @@
 from relationship_app.models import Author, Book, Library, Librarian
-# List all books in a library
-Library.objects.get(name="library_name")
-#library_books = library.books.all()
 
-# Query all books by a specific author
-author = Author.objects.get(name="author_name")
+# -------- Query all books by a specific author --------
+author_name = "John Doe"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
 
+# -------- List all books in a library (CHECKER EXPECTS THIS) --------
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+library_books = library.books.all()
 
-# Retrieve the librarian for a library
+# -------- Retrieve the librarian for a library --------
 librarian = Librarian.objects.get(library=library)
